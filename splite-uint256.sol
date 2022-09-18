b4ef7b3f26fe093f6441c4a470d0d6eaab1f642a
Görkem YAVUZ <85451018+grkmyvz@users.noreply.github.com>
2022-09-18 19:54:56 +0300

    Update splite-uint256.sol

--- splite-uint256.sol
+++ splite-uint256.sol
@@ -15,10 +15,10 @@ contract SpliteNumber{
     function splitNumber() public { // number split function
         val1 = number % 1000; // We find the remainder after dividing the number by 1000.
         
-        uint256 number2 = (number - val1) / 1000; // Subtract val1 from number and divide by 1000. this allows us to delete the last 3 digits of the main number.
+        uint256 number2 = number / 1000; // Subtract val1 from number and divide by 1000. this allows us to delete the last 3 digits of the main number.
         val2 = number2 % 1000; // We find the remainder again from dividing the remainder number2 by 1000.
 
-        uint256 number3 = (number2 - val2) / 1000;
+        uint256 number3 = number2 / 1000;
         val3 = number3 % 1000;
     }
 }  
